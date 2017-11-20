fec20a583f86988602d10078d616193c4c79cad7
Lee <m.y.y.0326.cg@gmail.com>
2017-11-20 15:01:17 +0900

    [yuto-hara]user_informationテーブルを作りました

--- db/schema.rb
+++ db/schema.rb
@@ -26,4 +26,13 @@ ActiveRecord::Schema.define(version: 20171120055308) do
     t.datetime "updated_at", null: false
   end
 
+
+  create_table "user_informations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.integer "tel"
+    t.string "gmail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
