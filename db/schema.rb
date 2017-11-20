42353eaf859fe06f431d9a3b727459104e4a0065
Lee <m.y.y.0326.cg@gmail.com>
2017-11-20 17:22:30 +0900

    [yuto/users#new]ユーザー入力完成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120055308) do
+ActiveRecord::Schema.define(version: 20171120081626) do
 
   create_table "connect_histories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
@@ -27,10 +27,19 @@ ActiveRecord::Schema.define(version: 20171120055308) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "user_informations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id"
+    t.integer "tel"
+    t.string "gmail"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "uuid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
