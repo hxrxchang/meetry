1734c32105de3bd29d7b1a8ed87db2fbaff8b3b4
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-11-20 15:15:22 +0900

    Update pair_image_id

--- db/migrate/20171120052341_create_connect_histories.rb
+++ db/migrate/20171120052341_create_connect_histories.rb
@@ -3,6 +3,7 @@ class CreateConnectHistories < ActiveRecord::Migration[5.1]
     create_table :connect_histories do |t|
       t.integer :user_id, null: false
       t.integer :user_id, null: false
+      t.integer :pair_image_id, null: false
       t.timestamps
     end
   end
