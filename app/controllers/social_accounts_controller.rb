3745f0ec10583a38f4823b9adf8b45eb5ca79791
kazumalab <kazuma-arashi@hotmail.co.jp>
2017-11-21 14:47:06 +0900

    Add: Social Account Create

--- app/controllers/social_accounts_controller.rb
+++ app/controllers/social_accounts_controller.rb
@@ -1,5 +1,18 @@
 class SocialAccountsController < ApplicationController
-  def new
-    @social_account = SocialAccount.new
+  def create
+    social_account = SocialAccount.new(social_params)
+    social_account.user = current_user
+    social_account.save!
+  end
+
+  def destroy
+    SocialAccount.find(params[:id]).destroy
+    flash[:success] = "success deleted!"
+  end
+
+  private
+
+  def social_params
+    params.require(:social_account).permit(:provider, :url)
   end
 end
